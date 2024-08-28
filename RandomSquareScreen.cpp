b713264c67eb73f66a7bf9a742b317c41fe5abde
km4kfl <lkmcguire@proton.me>
2024-08-28 17:21:51 -0400

    Reworked as random square screen.

--- RandomSquareScreen.cpp
+++ RandomSquareScreen.cpp
@@ -143,88 +143,25 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
-   SetTimer(hWnd, 100, 5000, (TIMERPROC)NULL);
-   SetTimer(hWnd, 101, 1000, (TIMERPROC)NULL);
+   SetTimer(hWnd, 100, 1000, (TIMERPROC)NULL);
+   //SetTimer(hWnd, 101, 1000, (TIMERPROC)NULL);
    return TRUE;
 }
 
 static void UpdateScreen(HWND hwnd, HDC hdc, PAINTSTRUCT &ps) {
-    SmartPrivateHDC mem_hdc(SmartHDCInner(hwnd, CreateCompatibleDC(hdc)));
-
-    // sometimes hdc can be NULL or did resources run out?? resource leak?
-    // likely a resource leak
-    if (mem_hdc.GetHandle().hdc == NULL) {
-        return;
-    }
-
-    //BOOL_THROW(mem_hdc.GetHandle().hdc != NULL);
-
     RECT win_rect;
     GetClientRect(hwnd, &win_rect);
 
     LONG width = win_rect.right - win_rect.left;
     LONG height = win_rect.bottom - win_rect.top;
 
-    SmartBitmap hbm_win(CreateCompatibleBitmap(
-        hdc,
-        width,
-        height
-    ));
-
-    BOOL_THROW(hbm_win.GetHandle() != NULL);
-
-    SmartGdiObj sgdi0(SmartGdiObjInner(
-        mem_hdc.GetHandle().hdc,
-        SelectObject(mem_hdc.GetHandle().hdc, hbm_win.GetHandle())
-    ));
-
-    int r2 = BitBlt(mem_hdc.GetHandle().hdc, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
-
-    BITMAP bmp;
-
-    BOOL_THROW(GetObject(hbm_win.GetHandle(), sizeof(bmp), &bmp) != 0);
-
-    BITMAPINFOHEADER bmp_hdr;
-
-    SIZE_T bmp_bit_count = 32;
-
-    SIZE_T bmp_size = ((bmp.bmWidth * bmp_bit_count + 31) / 32) * 4 * bmp.bmHeight;
-
-    SmartGlobalAlloc h_data(GlobalAlloc(GHND, bmp_size));
-
-    BOOL_THROW(h_data.GetHandle() != NULL);
-
-    unsigned char* p_data = (unsigned char*)GlobalLock(h_data.GetHandle());
-
-    BOOL_THROW(p_data != NULL);
-
-    memset(&bmp_hdr, 0, sizeof(bmp_hdr));
-
-    bmp_hdr.biSize = sizeof(BITMAPINFOHEADER);
-    bmp_hdr.biWidth = bmp.bmWidth;
-    bmp_hdr.biHeight = bmp.bmHeight;
-    bmp_hdr.biPlanes = 1;
-    bmp_hdr.biBitCount = 32;
-    bmp_hdr.biCompression = BI_RGB;
-
-    int r0 = GetDIBits(
-        mem_hdc.GetHandle().hdc,
-        hbm_win.GetHandle(),
-        0,
-        bmp.bmHeight,
-        p_data,
-        (BITMAPINFO*)&bmp_hdr,
-        DIB_RGB_COLORS
-    );
+    float aw = 55;
+    float ah = 55;
 
     std::random_device rd;
     std::mt19937 g(rd());
 
-    /*
-    float aw = 55;
-    float ah = 55;
-
-    for (int x = 0; x < 1; ++x) {
+    for (int x = 0; x < 400; ++x) {
         unsigned char rb = g() % 256;
         unsigned char gb = g() % 256;
         unsigned char bb = g() % 256;
@@ -233,57 +170,12 @@ static void UpdateScreen(HWND hwnd, HDC hdc, PAINTSTRUCT &ps) {
         memset(&_rect, 0, sizeof(_rect));
         _rect.top = g() % height;
         _rect.left = g() % width;
-        _rect.right = _rect.left + (double)g() / (double)0xffffffff * aw;
-        _rect.bottom = _rect.top + (double)g() / (double)0xffffffff * ah;
+        double pr = (double)g() / (double)0xffffffff;
+        double pb = (double)g() / (double)0xffffffff;
+        _rect.right = _rect.left + pr * aw;
+        _rect.bottom = _rect.top + pb * ah;
         FillRect(hdc, &_rect, scb0.GetHandle());
     }
-    */
-
-    unsigned int _g_max = 0;
-
-    SIZE_T pixel_count = bmp_size / 4;
-    
-    for (SIZE_T x = 0; x < pixel_count; ++x) {
-        unsigned char cr = g() & 0xff;
-        unsigned char cg = g() & 0xff;
-        unsigned char cb = g() & 0xff;
-        p_data[x * 4 + 0] = cr;
-        p_data[x * 4 + 1] = cg;
-        p_data[x * 4 + 2] = cb;
-        p_data[x * 4 + 3] = 0;
-    }
-
-    SmartBitmap hbm_tmp(CreateCompatibleBitmap(
-        mem_hdc.GetHandle().hdc,
-        width,
-        height
-    ));
-
-    int r6 = SetDIBits(
-        NULL, // only used if DIB_PAL_COLORS is used
-        hbm_tmp.GetHandle(),
-        0,
-        bmp.bmHeight,
-        p_data,
-        (BITMAPINFO*)&bmp_hdr,
-        DIB_RGB_COLORS
-    );
-
-    h_data.Dealloc();
-
-    SmartGdiObj sgdi1(SmartGdiObjInner(
-        mem_hdc.GetHandle().hdc,
-        SelectObject(mem_hdc.GetHandle().hdc, hbm_tmp.GetHandle())
-    ));
-
-    int r4 = BitBlt(hdc, 0, 0, width, height, mem_hdc.GetHandle().hdc, 0, 0, SRCCOPY);
-
-    // NOTE: These select the old objects when deconstructed as per the win32 API documentation.
-    sgdi0.Dealloc();
-    sgdi1.Dealloc();
-    h_data.Dealloc();
-    mem_hdc.Dealloc();
-    hbm_win.Dealloc();
 }
 
 //
@@ -318,6 +210,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 break;
             }
         }
+        break;
     }
     case WM_COMMAND:
     {
